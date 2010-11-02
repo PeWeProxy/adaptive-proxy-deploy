47cd8e1677bc0b2240affbd924595a1b9fdd8b4c
Jan Hudek <hudek.jan@gmail.com>
2010-11-02 12:59:23 +0100

    Delete adaptive-proxy-core directory
    Submodule will be used instead.

--- adaptive-proxy-core/migrations/019_add_result_position.rb
+++
@@ -1,9 +0,0 @@
-class AddResultPosition < ActiveRecord::Migration
-  def self.up
-    add_column :bf_recommendations, :position, :integer
-  end
-
-  def self.down
-    remove_column :bf_recommendations, :position
-  end
-end
