131a3f8bd5f1c637eeb4a767bf054a65d62e3e0d
Tomas Kramar <kramar.tomas@gmail.com>
2011-05-10 00:21:16 +0200

    Update production repository paths

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,11 @@
 set :deploy_to, "/home/peweproxy/release"
-set :repository,  "gitosis@relax.fiit.stuba.sk:adaptive-proxy-maintenance.git"
+set :repository,  "git://gitbus.fiit.stuba.sk/pewe-proxy-team-project/adaptive-proxy-deploy.git"
 #stage-specific, moved to config/deploy/staging and config/deploy/production.rb
 #set :deploy_to, "/var/rails/#{application}"
 server "peweproxy.fiit.stuba.sk", :app, :web, :db, :primary => true
 
 set :git_branch, "master"
-set :git_server, "relax.fiit.stuba.sk"
+set :git_server, "peweproxy-staging.fiit.stuba.sk"
 
 def run_migrations
   run "cd #{current_path}/schema/ && rake migrate RAILS_ENV='production'"
